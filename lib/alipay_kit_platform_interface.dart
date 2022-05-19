1380b4bb89c17b707dfdf73dc723c9496a999314
v7lin <v7lin@qq.com>
2022-05-19 09:47:20 +0800

    fix

--- lib/alipay_kit_platform_interface.dart
+++ lib/alipay_kit_platform_interface.dart
@@ -1,1 +1,7 @@
 library alipay_kit_platform_interface;
+
+export 'src/alipay_kit_method_channel.dart';
+export 'src/alipay_kit_platform_interface.dart';
+export 'src/json/jser_converter.dart';
+export 'src/model/alipay_resp.dart';
+export 'src/model/auth_result.dart';
--- lib/src/alipay.dart
+++ lib/src/alipay.dart
@@ -4,7 +4,7 @@ import 'package:alipay_kit/src/model/alipay_resp.dart';
 /// 支付宝
 ///
 /// * 默认包含支付，参考 https://github.com/RxReader/alipay_kit/blob/master/example/ios/Podfile
-///   修改 `$WechatKitSubspec = 'no_pay'` 可切换为不包含支付。
+///   修改 `$AlipayKitSubspec = 'bdr'` 可切换为不包含支付。
 /// * 不含「iOS 支付」调用会抛出 [MissingPluginException]。
 class Alipay {
   const Alipay._();
